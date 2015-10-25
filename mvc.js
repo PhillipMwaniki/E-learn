124dcb1f3ee7417485505e9e7635ef9ea640ea18
AndrewKralovec <akrala@siu.edu>
2015-10-25 15:06:25 -0500

    Working mongo db admin

--- mvc.js
+++ mvc.js
@@ -5,9 +5,10 @@ var path = require('path'); //core module
 // "username:password@example.com/contactlist" 
 var databaseUrl = "localhost:27017/DB"; // default env
 // not needed, but emulates a MongoDB JavaScript client 
-//var collections = ["users", "reports"]
+//var collections = ["DB", "contact"]
 var mongojs = require("mongojs");
 var db = mongojs("DB",['contact']); 
+var bodyParser = require('body-parser');
 
 //configure app
 var app = express(); 
@@ -22,27 +23,80 @@ db.on('connect', function () {
 
 // store all html files in views 
 app.use(express.static(__dirname + '/views'));
+// parses recived json input 
+app.use(bodyParser.json());
 // store all js in Scripts folder
 app.use(express.static(__dirname + '/scripts'));
 
-// listen for contactlist get request 
-app.get('/contactlist', function(req,res){
-	 console.log("recived GET request "); 
-
-	 db.contact.find(function(err,docs){
-	 	if(err)
-	 		{throw err ; }
-	 	console.log("docs request ");
-	 	console.log(docs);
-	 	res.json(docs); 
-	 }); 
-}); 
-	
-
 // Technology not needed but good practice, especailly when serval people are working on it 
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
 	res.sendFile('index.html');
 }); 
+
+// listen for contactlist get request, aka transfers the contacklist in mongo to client
+app.get('/contactlist', function (req, res) {
+	 console.log("-- recived GET request --"); 
+
+	 db.contact.find(function(err, docs){
+	 	if(err)
+	 		{throw err ; }
+	 	console.log("-- contactlist docs --");
+	 	console.log(docs);
+	 	res.json(docs); 
+	 	console.log("-- result: "+docs.length+" docs found");
+	 }); 
+}); 
+
+// listen for post request for body from global controller 
+app.post('/contactlist', function (req, res) {
+	console.log(" insert request ");
+	console.log(req.body);
+	db.contact.insert(req.body, function(err, docs) {
+		if(err) {throw err ; }
+		res.json(docs);
+		console.log("-- inserted --");
+  });
+});
+
+// delete request
+app.delete('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log(" delete request ");
+	db.contact.remove({_id: mongojs.ObjectId(id)},
+		function (err, doc) {
+			if(err) {throw err ; }
+			res.json(doc);
+			console.log("deleted id: "+id);
+
+	});
+
+});
+
+// Respond to the edit get request 
+app.get('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log("edit request for id: "+id);
+	db.contact.findOne({_id: mongojs.ObjectId(id)},
+		function (err, doc) {
+			if(err) {throw err ; }
+			res.json(doc);
+			console.log("loaded id: "+id);
+	});
+});
+
+app.put('/contactlist/:id', function (req, res) {
+	var id = req.params.id;
+	console.log("update recived for id: "+id);
+	db.contact.findAndModify({
+		query: {_id: mongojs.ObjectId(id)},
+		update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+			new: true}, function (err, doc) {
+				if(err) {throw err ; }
+				res.json(doc);
+				console.log(req.body.name+"\n updated");
+    		}
+	);
+});
 	
 
 // Implement a web server to listen to requests 
--- views/js/global.js
+++ views/js/global.js
@@ -1,12 +1,58 @@
 var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function($scope, $http) {
+	console.log("controller connected");
 
-	// create contacklist route 
- $http.get('/contactlist').success(function(response) {
+function refresh(){ 
+// create contacklist route 
+$http.get('/contactlist').success(function(response) {
     console.log("recived data requested");
      $scope.contactlist = response;
+     $scope.contact = "";
   });
+}
+
+// Call refresh to init cantacklist 
+refresh(); 
+
+// add Doc to table 
+$scope.addContact = function() {
+	console.log($scope.contact);
+    $http.post('/contactlist', $scope.contact).success(function(response) {
+    	console.log(response);
+    	refresh(); 
+  		});
+	};
+
+// remove Doc from table
+$scope.remove = function(id) {
+	console.log("request id");
+  console.log(id);
+  $http.delete('/contactlist/' + id).success(function(response) {
+    refresh();
+  });
+};
+
+// put response into the input boxes 
+$scope.edit = function(id) {
+  console.log(id);
+  $http.get('/contactlist/'+id).success(function(response) {
+    $scope.contact = response;
+  });
+}; 
 
 
-});
+$scope.update = function() {
+  console.log($scope.contact._id);
+  $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response) {
+    refresh();
+  })
+};
+
+$scope.deselect = function() {
+  $scope.contact = "";
+}
+
+
+
+});// Controller 
